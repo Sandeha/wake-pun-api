ed3c694d937f6136236e983b9c2e786ad02f862f
chris-malloy <chrismalloymusic@gmail.com>
2020-09-21 19:47:27 -0400

    Adding wake route.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
+var alan = require('../config/alan')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/wake', function (req, res, next) {
+  const randomString = alan[Math.floor(Math.random() * alan.length)];
+  res.json({ randomString })
 });
 
 module.exports = router;
